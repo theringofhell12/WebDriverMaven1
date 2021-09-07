6e538e72c7dcced9fb316225dc607d1be3406c04
Do Thi Thanh Thanh <thanh.dothithanh@petronas.com>
2021-09-07 15:16:30 +0700

    Initial Commit

---
+++ src/test/java/defaultPackage/LoginTest.java
@@ -0,0 +1,34 @@
+package defaultPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("trainer@wayautomation");
+		driver.findElement(By.id("Passwd")).sendKeys("sdfsdr");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
